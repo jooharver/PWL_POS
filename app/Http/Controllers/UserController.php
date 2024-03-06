1ec51c594d63683f4eac503db09f1520fb866a82
Eka Krisna Ferian <kresnaeka.07@gmail.com>
2024-03-06 10:19:03 +0700

    fungsi firstOrCreate dan firstOrNew

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,18 +10,15 @@ class UserController extends Controller
 {
     public function index(){
 
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-
-        // UserModel::create($data);
-
-        $jumlahPengguna = UserModel::where('level_id', 2)->count();
-        dd($jumlahPengguna);
-       
-        return view('user', ['data'=> $jumlahPengguna]);
+       $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+       ],
+        );
+        $user->save();
+    return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,19 +6,19 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                {{-- <th>ID</th>
+                <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
-                <th>ID Level Pengguna</th> --}}
-                <th>Jumlah Pengguna</th>
+                <th>ID Level Pengguna</th>
+               
             </tr>
             {{-- @foreach ($data as $user) --}}
             <tr>
-                {{-- <td>{{$user->user_id}}</td>
-                <td>{{$user->username}}</td>
-                <td>{{$user->nama}}</td>
-                <td>{{$user->level_id}}</td> --}}
-                <td>{{$data}}</td>
+                <td>{{$data->user_id}}</td>
+                <td>{{$data->username}}</td>
+                <td>{{$data->nama}}</td>
+                <td>{{$data->level_id}}</td>
+                
             </tr>
             {{-- @endforeach --}}
         </table>
