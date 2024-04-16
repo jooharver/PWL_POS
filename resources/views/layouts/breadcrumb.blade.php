5e8871b5838d972a1fb82ffc8de63bb1dd2278a8
Eka Krisna Ferian <kresnaeka.07@gmail.com>
2024-04-16 21:28:18 +0700

    Jobsheet 7 Praktikum 2 – Penerapan Layouting

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -2,13 +2,18 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Blank Page</h1>
+        <h1>{{ $breadcumb->title }}</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
-            <li class="breadcrumb-item active">Blank Page</li>
-          </ol>
+                @foreach($breadcumb->list as $key => $value)
+                        @if($key == count($breadcumb->list) - 1)
+                            <li class="breadcrumb-item active">{{ $value }}</li>
+                        @else
+                            <li class="breadcrumb-item">{{ $value }}</li>
+                        @endif
+                @endforeach
+           </ol>
         </div>
       </div>
     </div><!-- /.container-fluid -->
