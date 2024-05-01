0874dc4f50dfee734e08563884b4f2e0d9a258ba
Eka Krisna Ferian <kresnaeka.07@gmail.com>
2024-05-01 19:34:58 +0700

    Jobsheet 7 Praktikum 3 – Implementasi jQuery Datatable di AdminLTE

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,19 +5,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name', 'PWL Laravel Starter Code') }}</title>
 
+  <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Untuk mengirimkan token Laravel CSRF pada setiap request ajax -->
+
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- Data Tables -->
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+
+  @stack('css') <!-- Digunakan untuk memanggil custom css dari perintah push(css) pada masing-masing view -->
 </head>
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
   <!-- Navbar -->
-  @include('layouts.header')
-  <!-- /.navbar -->
+ @include('layouts.header')
+ <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
@@ -28,7 +36,7 @@
     </a>
 
     <!-- Sidebar -->
-   @include('layouts.sidebar')
+    @include('layouts.sidebar')
     <!-- /.sidebar -->
   </aside>
 
@@ -36,6 +44,7 @@
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     @include('layouts.breadcrumb')
+
     <!-- Main content -->
     <section class="content">
         @yield('content')
@@ -43,8 +52,8 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
+
   @include('layouts.footer')
-  <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
@@ -52,7 +61,24 @@
 <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- DataTables dan Plugins -->
+<script src="{{asset('adminlte/plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/pdfmake/vfs_font.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{asset('adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+<script>
+  $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
+</script>
+@stack('js')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -9,40 +9,36 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\WelcomeController;
 
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-//Praktikum 4
-// Route::get('/level', [LevelController::class, 'index']);
-//praktikum 5
-// Route::get('/kategori', [KategoriController::class, 'index']);
+// Praktikum 4
+Route::get('/level', [LevelController::class, 'index']);
+// praktikum 5
+Route::get('/kategori', [KategoriController::class, 'index']);
 
-// Route::get('/user', [UserController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 
-// Route::get('/user/tambah', [UserController::class, 'tambah']);
-
-
-
-// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/user/tambah', [UserController::class, 'tambah']);
 
 
-// Route::get('/kategori', [KategoriController::class, 'index']);
-// Route::get('/kategori/create', [KategoriController::class, 'create']);
-// Route::post('/kategori', [KategoriController::class, 'store']);
-// Route::get('/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
-// Route::put('/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-// Route::get('/delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.delete');
 
-//Manage User
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::post('/kategori', [KategoriController::class, 'store']);
+Route::get('/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+Route::put('/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+Route::get('/delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.delete');
+
+// Manage User
 // Route::get('/user/create', [UserController::class, 'create'])->name('/user/create');
 // Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('/user/edit');
 // Route::get('/user', [UserController :: class, 'index'])->name('user.index');
@@ -50,15 +46,15 @@ use App\Http\Controllers\WelcomeController;
 // Route::put('/user/{id}', [UserController :: class, 'edit_simpan'])->name('/user/edit_simpan');
 // Route::get('/user/delete/{id}', [UserController::class, 'delete'])->name('/user/delete');
 
-// // Manage Level
-// Route::get('/level', [LevelController::class, 'index'])->name('level.index');
-// Route::get('/level/create', [LevelController::class, 'create'])->name('/level/create');
-// Route::post('/level', [LevelController::class, 'store']);
-// Route::get('/level/edit/{id}', [LevelController::class, 'edit'])->name('/level/edit');
-// Route::put('/level/{id}', [LevelController::class, 'edit_simpan'])->name('/level/edit_simpan');
-// Route::get('/level/delete/{id}', [LevelController::class, 'delete'])->name('/level/delete');
+// Manage Level
+Route::get('/level', [LevelController::class, 'index'])->name('level.index');
+Route::get('/level/create', [LevelController::class, 'create'])->name('/level/create');
+Route::post('/level', [LevelController::class, 'store']);
+Route::get('/level/edit/{id}', [LevelController::class, 'edit'])->name('/level/edit');
+Route::put('/level/{id}', [LevelController::class, 'edit_simpan'])->name('/level/edit_simpan');
+Route::get('/level/delete/{id}', [LevelController::class, 'delete'])->name('/level/delete');
 
-// Route::resource('m_user', POSController::class );
+Route::resource('m_user', POSController::class );
 
 // Manage Level
 // Route::get('/level', [LevelController::class, 'index'])->name('level.index');
@@ -69,3 +65,14 @@ use App\Http\Controllers\WelcomeController;
 // Route::get('/level/delete/{id}', [LevelController::class, 'delete'])->name('level.delete');
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
